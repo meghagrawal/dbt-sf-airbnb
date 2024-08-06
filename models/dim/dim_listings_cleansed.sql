cbe1229f04ca34a4f83a845f29e2e14aefa156c3
Megha <agrawalmegha360@gmail.com>
2024-08-06 11:15:45 +0530

    fact models and materialisation changes

--- models/dim/dim_listings_cleansed.sql
+++ models/dim/dim_listings_cleansed.sql
@@ -1,9 +1,15 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+
 with src_listings as 
 (
     select * from {{ ref('src_listings')}}
 )
 select 
-listing_id,
+listing_id, 
   listing_name,
   room_type,
   CASE
