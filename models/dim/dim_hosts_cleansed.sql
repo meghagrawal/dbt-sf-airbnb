fc1e0430ab5b3b7e42a69e5760e32536562bc069
Megha <agrawalmegha360@gmail.com>
2024-08-06 09:30:23 +0530

    src and dim models

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,13 @@
+with src_hosts as
+(
+    SELECT 
+    * 
+    FROM {{ ref('src_hosts')}}
+)
+SELECT 
+HOST_ID
+,NVL(HOST_NAME, 'Anonymous') AS HOST_NAME
+,IS_SUPERHOST
+,CREATED_AT
+,UPDATED_AT
+FROM  src_hosts
